dbbdaadbcd896b1e4ddbb9341e0a3296292b3a9d
Mazedur (ISRT, DU) <89848156+LaraXpress@users.noreply.github.com>
2022-06-02 13:52:38 +0600

    configure environment variable

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.get('/',(req,res)=>{
+require('dotenv/config');
+const api = process.env.API_URL;
+
+app.get(api+'/products',(req,res)=>{    
     res.send("Hello Mazed, Welcome to Node JS in Bangladesh");
 });
+
 app.listen(3000,()=>{
+    console.log(api);
     console.log('Server is running at http://localhost:3000');
 });
