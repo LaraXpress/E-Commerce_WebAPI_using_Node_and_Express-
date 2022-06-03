b6df7fe382031ba9638a0a52c4e9287e945705f6
Mazedur (ISRT, DU) <89848156+LaraXpress@users.noreply.github.com>
2022-06-03 14:37:38 +0600

    Mongoose and Connect to MongoDB Database

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 //middleware
 app.use(bodyParser.json());
+app.use(morgan('tiny'));
 
 require('dotenv/config');
 const api = process.env.API_URL;
+
+mongoose.connect(process.env.CONNECTION_STRING)
+.then(()=>{
+    console.log('Database connection is ready to use');
+})
+.catch((error)=>{
+    console.log(error);
+})
+
+const productSchema = mongoose.Schema({
+    name:String,
+    image:String,
+    countInStock:Number
+})
+const Product = mongoose.model('Product',productSchema);
 
 app.get(`${api}/products`,(req,res)=>{    
     const product={
@@ -18,9 +36,20 @@ app.get(`${api}/products`,(req,res)=>{
 });
 
 app.post(`${api}/products`,(req,res)=>{    
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct);
+    
+    const product = new Product({
+        name:req.body.name,
+        image:req.body.image,
+        countInStock:req.body.countInStock
+    })     
+    product.save().then((createProduct=>{
+        res.status(201).json(createProduct)
+    })).catch((err)=>{
+        res.status(501).json({
+            error:err,
+            success:false
+        })
+    })        
 });
 
 app.listen(3000,()=>{
