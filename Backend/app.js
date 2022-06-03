26de6449c0b500e62f4f5171bc87086367aa9e48
Mazedur (ISRT, DU) <89848156+LaraXpress@users.noreply.github.com>
2022-06-03 21:37:38 +0600

    modified app.js

--- Backend/app.js
+++ Backend/app.js
@@ -19,39 +19,6 @@ mongoose.connect(process.env.CONNECTION_STRING)
     console.log(error);
 })
 
-const productSchema = mongoose.Schema({
-    name:String,
-    image:String,
-    countInStock:Number
-})
-const Product = mongoose.model('Product',productSchema);
-
-app.get(`${api}/products`,(req,res)=>{    
-    const product={
-        id:1,
-        name:'John Smith',
-        profession :'Software Engineer'
-    }
-    res.send(product);
-});
-
-app.post(`${api}/products`,(req,res)=>{    
-    
-    const product = new Product({
-        name:req.body.name,
-        image:req.body.image,
-        countInStock:req.body.countInStock
-    })     
-    product.save().then((createProduct=>{
-        res.status(201).json(createProduct)
-    })).catch((err)=>{
-        res.status(501).json({
-            error:err,
-            success:false
-        })
-    })        
-});
-
 app.listen(3000,()=>{
     console.log(api);
     console.log('Server is running at http://localhost:3000');
